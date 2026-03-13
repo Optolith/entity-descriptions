7fa46928df8b4c229036b50e7c1a8f0718e6aa28
Lukas Obermann <lukas.obermann@icloud.com>
2026-03-13 23:00:33 +0100

    fix: render condition effects as table

--- src/entities/condition.ts
+++ src/entities/condition.ts
@@ -1,75 +1,68 @@
+import { romanize } from "@elyukai/utils/roman"
 import { createEntityDescriptionCreator } from "../creator.js"
 
 /**
  * Get a JSON representation of the rules text for a condition.
  */
-export const getConditionEntityDescription =
-  createEntityDescriptionCreator<"Condition">(
-    (_, { translate, translateMap }, { content: entry }) => {
-      const translation = translateMap(entry.translations)
+export const getConditionEntityDescription = createEntityDescriptionCreator<"Condition">(
+  (_, { translate, translateMap }, { content: entry }) => {
+    const translation = translateMap(entry.translations)
 
-      if (translation === undefined) {
-        return undefined
-      }
+    if (translation === undefined) {
+      return undefined
+    }
 
-      return {
-        title: translation.name,
-        className: "condition",
-        body: [
-          translation.rules === undefined
-            ? undefined
-            : { type: "plain", text: translation.rules },
-          {
-            type: "definitionList",
-            items: [
-              ...translation.effects.map((effect, index) => ({
-                label: translate("Level {$level}", {
-                  level: index + 1,
-                }),
-                value: effect,
-              })),
-            ],
-          },
-        ],
-        errata: translation.errata,
-        references: entry.src,
-      }
-    },
-  )
+    return {
+      title: translation.name,
+      className: "condition",
+      body: [
+        translation.rules === undefined ? undefined : { type: "plain", text: translation.rules },
+        {
+          type: "table",
+          header: [translate("Level"), translate("Effect")],
+          rows: translation.effects.map((effect, index) => [
+            translate("Level {$level}", {
+              level: romanize(index + 1),
+            }),
+            effect,
+          ]),
+        },
+      ],
+      errata: translation.errata,
+      references: entry.src,
+    }
+  },
+)
 
 /**
  * Get a JSON representation of the rules text for a meta condition.
  */
-export const getMetaConditionEntityDescription =
-  createEntityDescriptionCreator<"MetaCondition">(
-    (_, { translate, translateMap }, { content: entry }) => {
-      const translation = translateMap(entry.translations)
+export const getMetaConditionEntityDescription = createEntityDescriptionCreator<"MetaCondition">(
+  (_, { translate, translateMap }, { content: entry }) => {
+    const translation = translateMap(entry.translations)
 
-      if (translation === undefined) {
-        return undefined
-      }
+    if (translation === undefined) {
+      return undefined
+    }
 
-      return {
-        title: translation.name,
-        className: "meta-condition",
-        body: [
-          translation.rules === undefined
-            ? undefined
-            : { type: "plain", text: translation.rules },
-          {
-            type: "definitionList",
-            items: [
-              ...translation.effects.map((effect, index) => ({
-                label: translate("Level {$level}", {
-                  level: index + 1,
-                }),
-                value: effect,
-              })),
-            ],
-          },
-        ],
-        errata: translation.errata,
-        references: entry.src,
-      }
-    },
-  )
+    return {
+      title: translation.name,
+      className: "meta-condition",
+      body: [
+        translation.rules === undefined ? undefined : { type: "plain", text: translation.rules },
+        {
+          type: "table",
+          header: [translate("Level"), translate("Effect")],
+          rows: translation.effects.map((effect, index) => [
+            translate("Level {$level}", {
+              level: romanize(index + 1),
+            }),
+            effect,
+          ]),
+        },
+      ],
+      errata: translation.errata,
+      references: entry.src,
+    }
+  },
+)
