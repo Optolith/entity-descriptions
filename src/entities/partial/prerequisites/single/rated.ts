6f258b1f6f4fb1c88b09fe9fb2013b180d91fc4b
Lukas Obermann <lukas.obermann@icloud.com>
2026-03-18 15:44:52 +0100

    fix: render attribute prerequisite as abbreviation

--- src/entities/partial/prerequisites/single/rated.ts
+++ src/entities/partial/prerequisites/single/rated.ts
@@ -1,7 +1,7 @@
 import type { RatedPrerequisite } from "optolith-database-schema/gen"
 import { type GetInstanceById } from "../../../../helpers/getTypes.js"
 import type { LocaleEnvironment } from "../../../../helpers/locale.js"
-import { attributedName } from "../../markdown.js"
+import { attributedCustomName } from "../../markdown.js"
 import { MISSING_VALUE } from "../../unknown.js"
 import { printDisplayOption } from "../displayOption.js"
 import type { PrerequisitePart } from "../part.js"
@@ -28,7 +28,7 @@ export const printRatedPrerequisite = (
   }
 
   return {
-    value: `${attributedName(locale.translateMap, getInstanceById, "prerequisite", prerequisite.id) ?? MISSING_VALUE} ${prerequisite.value.toFixed()}`,
+    value: `${attributedCustomName(locale.translateMap, getInstanceById, "prerequisite", (t: { name: string; abbreviation?: string }) => t.abbreviation ?? t.name, prerequisite.id) ?? MISSING_VALUE} ${prerequisite.value.toFixed()}`,
     sentenceType: undefined,
     isMeta: false,
   }
