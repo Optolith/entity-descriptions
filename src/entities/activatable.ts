2d07eff1d4b6a5b72ce705e24969e4a384a1d486
Lukas Obermann <lukas.obermann@icloud.com>
2026-06-07 13:31:22 +0200

    fix: display full tradition name

--- src/entities/activatable.ts
+++ src/entities/activatable.ts
@@ -1435,9 +1435,14 @@ export const getActivatableEntityDescription = createEntityDescriptionCreator<
             ).run(env)
           : []
 
+    const makeTraditionName: (name: string) => string =
+      entityName === "BlessedTradition" || entityName === "MagicalTradition"
+        ? name => translate("Tradition ({$tradition})", { tradition: name })
+        : name => name
+
     return {
       title:
-        (translation.name_in_library ?? translation.name) +
+        makeTraditionName(translation.name_in_library ?? translation.name) +
         (baseEntry.levels !== undefined ? ` I–${romanize(baseEntry.levels)}` : ""),
       subtitle: mapNullable(baseEntry.usage_type, usageType => {
         switch (usageType.kind) {
